6c08709f0e69d07c6afba57ba798c6b5bdf3ad9b
NFTwolfmaximus <wolfmaximus.live@gmail.com>
2023-04-11 23:08:42 -0500

    Removing unneccesary imports and empty files

--- pages/collectionsShowcase.tsx
+++ pages/collectionsShowcase.tsx
@@ -1,19 +1,11 @@
-import { useContract, useNFTs } from "@thirdweb-dev/react";
 import React from "react";
 import Container from "../components/Container/Container";
-import NFTGrid from "../components/NFT/NFTGrid";
 
 import SearchBar from "../components/SearchBar/SearchBar";
 import FeaturedCollections from "../components/FeaturedCollections/FeaturedCollections";
 import DisplayCollections from "../components/DisplayCollections/DisplayCollections";
 
-import { NFT_COLLECTION_ADDRESS } from "../const/contractAddresses";
-
 export default function collectionsShowcase() {
-    // Load all of the NFTs from the NFT Collection
-    const { contract } = useContract(NFT_COLLECTION_ADDRESS);
-    const { data, isLoading } = useNFTs(contract);
-
     return (
         <Container maxWidth="lg">
             <h1>Collections</h1>
