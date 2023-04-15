527f8549aab9f6d6c490918dd1db5e99df48d4fb
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-04-15 17:09:49 -0500

    Update embed.tsx

--- pages/embed.tsx
+++ pages/embed.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 const EmbedPage = () => {
     return (
         <div>
-            <h1>Need to revoke permissions?</h1>
+            <h1 style={{ textAlign: 'center' }}>Need to revoke permissions?</h1>
+            <br />
+
+            <h1 style={{ textAlign: 'center' }}>Need to revoke permissions?</h1>
             <iframe
                 src="https://revoke.cash/"
                 width="100%"
-                height="600"
+                height="800"
                 frameBorder="0"
                 allowFullScreen
                 title="Embedded Site"
