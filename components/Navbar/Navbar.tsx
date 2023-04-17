bafc3351fe87a155c245903f065a675058a6dcac
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-04-16 20:20:38 -0500

    Update Navbar.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -49,11 +49,6 @@ export function Navbar() {
                   </Link>
                 </Dropdown.Item>
                 <Dropdown.Item>
-                  <Link href="/ZoeClaim" className={styles.link}>
-                    Zoe Claim
-                  </Link>
-                </Dropdown.Item>
-                <Dropdown.Item>
                   <Link href="/earn" className={styles.link}>
                     Earn
                   </Link>
